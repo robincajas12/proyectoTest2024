b559d0b9e1f9cf3951e5bd1b58ee5d03b4a043b5
robincajas12 <robincajas12@gmail.com>
2024-05-18 02:35:07 +0000

    adding userProfile

--- typescript/controllers/profileController.ts
+++ typescript/controllers/profileController.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { Request, Response } from "express";
-
+import User from '../models/userInfoModel.js';
 const profileRouter =  express.Router();
 profileRouter.route('/').get(getProfile);
 
 function getProfile(req: Request, res: Response) {
-    res.render('');
+    res.render('pages/profile/userProfile',  
+    {
+        stylesheet: 'main', 
+        user : new User('unknown', {country:'EC', gender: 'Masculine'}, 'admin')});
 }
 
 export default profileRouter;
